56f57511d049aab774e6e32d5ca08721fe3ef1f0
Shivambembey98 <shivam.bembey@bellblazetech.com>
2025-05-27 16:37:35 +0530

    Update build.sh

--- PyProj/wrappers/build.sh
+++ PyProj/wrappers/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 echo "Building Python project..."
-mise use -y
+cd "$(dirname "$0")/.."  # Move from wrappers/ to project root
 pip install -r requirements.txt
 python setup.py build

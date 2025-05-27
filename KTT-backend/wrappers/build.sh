c8f6a59308d94af3df1eeffd83641ed8178e4ff4
Shivambembey98 <shivam.bembey@bellblazetech.com>
2025-05-27 15:51:25 +0530

    Update build.sh

--- KTT-backend/wrappers/build.sh
+++ KTT-backend/wrappers/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
+
 echo "Building Java project..."
-mise use -y
+
+# Point mise to repo root to read `.tool-versions` This ensures mise reads the correct .tool-versions file from root no matter where you run the script from.
+mise use -y --path "$(git rev-parse --show-toplevel)"
+
 mvn clean install

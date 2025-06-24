152bd08fa08cd0fc43762f0d0570f3e0c339cdc4
Shivambembey98 <shivam.bembey@bellblazetech.com>
2025-06-24 15:26:37 +0530

    Delete PyProj directory

--- PyProj/wrappers/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "Building Python project..."
-cd "$(dirname "$0")/.."  # Move from wrappers/ to project root
-pip install -r requirements.txt
-python setup.py build
